ce5616b3fafa26e8feba260db573df93d5a02141
ali <alirezakhlm10@gmail.com>
2023-12-06 11:52:19 +0330

    worked on user dashboard

--- user_panel_module/views.py
+++ user_panel_module/views.py
@@ -3,47 +3,55 @@ from django.http import HttpRequest
 
 from django.views import View
 
+from account_module.models import userModel
+
 # Create your views here.
 
 
 class userDashboardView(View):
-    def get(self, request : HttpRequest):
-        context = {}
-        return render(request,"user_panel_module/user_dashboard.html",context)
+    def get(self, request: HttpRequest):
+        current_user: userModel = userModel.objects.filter(
+            id=request.user.id).first()
+        context = {
+            "current_user": current_user,
+        }
+        return render(request, "user_panel_module/user_dashboard.html", context)
 
 
 class createPropertyView(View):
-    def get(self, request : HttpRequest):
+    def get(self, request: HttpRequest):
         context = {}
-        return render(request,"user_panel_module/create_property.html",context)
-    
-    
+        return render(request, "user_panel_module/create_property.html", context)
+
 
 class userPropertiesView(View):
-    def get(self, request : HttpRequest):
+    def get(self, request: HttpRequest):
         context = {}
-        return render(request,"user_panel_module/user_properties.html",context)
-    
+        return render(request, "user_panel_module/user_properties.html", context)
 
 
 class receivedRequestsView(View):
-    def get(self, request : HttpRequest):
+    def get(self, request: HttpRequest):
         context = {}
-        return render(request,"user_panel_module/received_requests.html",context)
-    
-    
-    
+        return render(request, "user_panel_module/received_requests.html", context)
+
+
 class sendedRequestsView(View):
-    def get(self, request : HttpRequest):
+    def get(self, request: HttpRequest):
         context = {}
-        return render(request,"user_panel_module/sended_requests.html",context)
+        return render(request, "user_panel_module/sended_requests.html", context)
 
 
 class userWalletView(View):
-    def get(self, request : HttpRequest):
+    def get(self, request: HttpRequest):
         context = {}
-        return render(request,"user_panel_module/user_wallet.html",context)
+        return render(request, "user_panel_module/user_wallet.html", context)
 
 
 def user_panel_menu_component(request: HttpRequest):
-    return render(request, 'user_panel_module/components/user_panel_menu_component.html')
+    current_user: userModel = userModel.objects.filter(
+        id=request.user.id).first()
+    context = {
+        "current_user": current_user,
+    }
+    return render(request, 'user_panel_module/components/user_panel_menu_component.html', context)
