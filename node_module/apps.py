0c16d80e24ad5da5dd044ab65898ed13ad39774c
ali <alirezakhlm10@gmail.com>
2023-12-13 18:43:17 +0330

    added new nodes to db and some changes happened

--- node_module/apps.py
+++ node_module/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class NodeModuleConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'node_module'
-    verbose_name = "ماژول ندها"
+    verbose_name = "ماژول گره ها"
--- node_module/models.py
+++ node_module/models.py
@@ -4,11 +4,11 @@ from django.db import models
 
 
 class nodeModel(models.Model):
-    node_name = models.CharField(verbose_name="نام گره", max_length=50)
-    node_port = models.CharField(verbose_name="شماره پورت", max_length=50)
-    node_url = models.URLField(verbose_name="آدرس گره", max_length=200)
+    node_name = models.CharField(verbose_name="نام گره", max_length=50,unique=True)
+    node_port = models.CharField(verbose_name="شماره پورت", max_length=50,unique=True)
+    node_url = models.URLField(verbose_name="آدرس گره", max_length=200,unique=True)
     node_inventory = models.FloatField(
-        verbose_name="موجودی حساب گره", default=0.0)
+        verbose_name="موجودی حساب گره", default=0.0,editable=False)
     node_join_to_network = models.DateTimeField(
         auto_now_add=True, editable=False, verbose_name="زمان ملحق شدن گره به شبکه", null=True, blank=True)
 
