3012cde2827745b46cdbeec376535c6ef0e59af5
ali <alirezakhlm10@gmail.com>
2023-12-13 18:36:30 +0330

    created new model for nodes and did migration on this model and some changes happened

--- node_module/admin.py
+++ node_module/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+
+class nodeAdmin(admin.ModelAdmin):
+    list_display = [
+        'node_name',
+        'node_port',
+        'node_url',
+    ]
+
+
+admin.site.register(models.nodeModel, nodeAdmin)
--- node_module/models.py
+++ node_module/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class nodeModel(models.Model):
+    node_name = models.CharField(verbose_name="نام گره", max_length=50)
+    node_port = models.CharField(verbose_name="شماره پورت", max_length=50)
+    node_url = models.URLField(verbose_name="آدرس گره", max_length=200)
+    node_inventory = models.FloatField(
+        verbose_name="موجودی حساب گره", default=0.0)
+    node_join_to_network = models.DateTimeField(
+        auto_now_add=True, editable=False, verbose_name="زمان ملحق شدن گره به شبکه", null=True, blank=True)
+
+    class Meta:
+        verbose_name = "گره"
+        verbose_name_plural = "گره ها"
+
+    def __str__(self):
+        return self.node_url
