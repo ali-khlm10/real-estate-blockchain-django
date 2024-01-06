7ba62ad6c5a97a0bfc3c2cfd579e88125b27abbd
ali <alirezakhlm10@gmail.com>
2024-01-06 21:25:16 +0330

    modified tokenization operation and delete some file and edit some file

--- static/js/custom_script.js
+++ static/js/custom_script.js
@@ -1,13 +1,12 @@
 $(document).ready(function () {
   $("#user").click(function (event) {
-    event.preventDefault(); // جلوگیری از انجام عملکرد پیش‌فرض لینک
-    $("#myDropdown").slideToggle(); // باز کردن یا بستن دراپ‌دان
+    event.preventDefault();
+    $("#myDropdown").slideToggle();
   });
 
 
   $(document).click(function (event) { 
     if (!$(event.target).closest('#user').length) {
-      // اگر روی دکمه کلیک نشده بود، دراپ‌دان را ببند
       $('#myDropdown').slideUp();
     }    
   });
--- static/js/signature_and_verify.js
+++
@@ -1,3 +0,0 @@
-// document.getElementById("signButton").addEventListener("click", async () => {
-//   console.log("clicked");
-// });
